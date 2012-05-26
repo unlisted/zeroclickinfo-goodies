d5675c015e479a0914d70a4563f28e4fb2af1a15
unlisted <dev@unlisted.org>
2012-05-26 09:59:02 -0400

    adding anagram solver

---
+++ lib/DDG/Goodie/AnagramSolver.pm
@@ -0,0 +1,55 @@
+package DDG::Goodie::AnagramSolver;
+# ABSTRACT: Solves anagrams
+
+use DDG::Goodie;
+
+use constant DICT_FILE => '/usr/share/dict/words';
+ 
+my %dict;
+sub create_dict {
+    open my $fh, '<', DICT_FILE
+        or die "Could not open DICTFILE\n";
+    while(<$fh>)
+    {
+        chomp;
+        my $word_index = join('', sort(split //, $_)); 
+        if (exists $dict{$word_index})
+        {
+            push @{$dict{$word_index}}, $_;
+        } else
+        {
+            my @word_list;
+            push @word_list, $_;
+            $dict{$word_index} = \@word_list;
+        }
+    }
+    close $fh;
+
+    # remove trivial cases
+#    foreach my $key (keys %dict) {
+#	delete $dict{$key} if (@{$dict{$key}} == 1);
+ #   }
+};
+
+triggers start => 'solve';
+
+handle remainder => sub {
+    print "$_\n";
+    my $key = join('', sort(split //, $_)); 
+    print "$key\n";
+
+    create_dict;
+    
+    return @{$dict{$key}} if exists $dict{$key};
+    return;
+
+    
+    #foreach my $key (keys %dict) {
+#	print "$key: ", scalar @{$dict{$key}}, "\n";
+ #   }
+
+};
+
+zci is_cached => 1;
+
+1;
